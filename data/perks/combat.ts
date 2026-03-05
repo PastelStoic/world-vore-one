e0c9f218f00614cbcb8f296e9fb4e6ffb18fda3c
DocDoc005 <doccerino@gmail.com>
2026-03-05 14:59:24 -0300

    Update ally cover mechanics in combat.ts
    Clarified the conditions for using an ally as cover in combat.

--- data/perks/combat.ts
+++ data/perks/combat.ts
@@ -180,7 +180,7 @@ export const COMBAT_PERKS: PerkDefinition[] = [
 
 *When being attacked, you may pull an ally in the same distance as you into the way. Does not apply if you were ambushed.
 *You may only protect yourself from a single attack per round, and it is a free action.
-*Your ally may be pulled before or after you perform your cover roll.
+*Your ally may be pulled before or after you perform your cover roll. If your ally is incapacitated, you must be actively holding them to use them as cover.
 *If your ally is willing, they take all of the damage in your stead.
 *If your ally is unwilling, you may do a STR vs STR contested check against them. On a success, they take all of the damage in your stead.
 *Officers and higher-ups can punish you for this behavior; it is great cowardice and poor conduct!`,

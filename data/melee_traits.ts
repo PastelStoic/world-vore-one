455e9377a147b16a0399f4cacabf56cac8a07cb0
DocDoc005 <doccerino@gmail.com>
2026-03-06 16:21:51 -0300

    Refine melee trait descriptions for clarity
    Updated descriptions for various melee traits to clarify effects and conditions.

--- data/melee_traits.ts
+++ data/melee_traits.ts
@@ -21,7 +21,7 @@ export const MELEE_TRAITS: MeleeTraitDefinition[] = [
     id: "bastard-weapon",
     name: "Bastard weapon",
     description:
-      "Can be used with either one hand or two hands.",
+      "Can be used with either one hand or two hands. +1 damage if used with two hands.",
   },
   {
     id: "crushing",
@@ -33,55 +33,67 @@ export const MELEE_TRAITS: MeleeTraitDefinition[] = [
     id: "extra-long",
     name: "Extra long",
     description:
-      "This melee weapon is much longer than its contemporaries. If attacked in melee, can use your action to attack them first, taking your action if it hasn't been used yet. Typical of spears, lances and pikes. -2d6 if used in cramped spaces (indoors, inside ships, tanks, etc.).",
+      "This melee weapon is much longer than its contemporaries. If attacked in melee, can use your action to attack them first, taking your action if it hasn't been used yet. -2d6 if used in cramped spaces (indoors, inside ships, tanks, etc.).",
+  },
+  {
+    id: "ridiculously-long",
+    name: "Ridiculously long",
+    description:
+      "Weapon is extremely long. Can attack enemies 1 distance away, but not on the same distance as you. Cannot be used in cramped spaces (indoors, inside ships, tanks, etc.).",
   },
   {
     id: "braceable",
     name: "Braceable",
     description:
-      `This melee weapon can be braced against incoming cavalry. If you brace, a mounted enemy or horse moving into distance 0 will take a fixed 3 damage and negates the "cavalry-weapon" trait of a weapon.`,
+      `This melee weapon can be braced against incoming cavalry. If you brace, a mounted enemy or horse moving into attacking distance will take an unavoidable instance of damage and negates the "cavalry-weapon" trait of a weapon.`,
   },
   {
     id: "flourishing",
     name: "Flourishing",
     description:
-      "This weapon can be used to make incredibly wide swings, which make it difficult for enemies to approach. If you flourish, an enemy moving into distance 0 will take an instance of damage. Typical of greatswords, zweihänders and other long bladed weapons. You cannot flourish in cramped spaces (indoors, inside ships, tanks, etc.).",
+      "This weapon can be used to make incredibly wide swings, which make it difficult for enemies to approach. If you flourish, an enemy moving into distance 0 will take an unavoidable instance of damage. You cannot flourish in cramped spaces (indoors, inside ships, tanks, etc.).",
   },
   {
     id: "rapid-shanking",
     name: "Rapid shanking",
     description:
-      "This weapon can be used to quickly shank enemies. If you choose to take a -2d6 when attacking, having an additional success doubles your damage. Weapons like these deal -1 damage than their tier would otherwise allow. Typical of small knifes and daggers.",
+      "This weapon can be used to quickly shank enemies. If you choose to take a -2d6 when attacking, having an additional success doubles your damage.",
   },
   {
     id: "duelling",
     name: "Duelling",
     description:
-      "Weapon is meant for duels and one-on-one's. If a single enemy is in melee range, have a +1d6 when fighting them. If there is more than one, have a -1d6 for each enemy. Typical of duelling swords, such as rapiers.",
+      "This weapon is meant for duels and one-on-one's. Only applies if you are holding nothing else but this weapon. If a single enemy is in melee range, gain +3d6 to attack them. If there is more than one, gain -1d6 to attack anyone for each enemy, up to -3d6.",
+  },
+  {
+    id: "parrying",
+    name: "Parrying",
+    description:
+      "This weapon is meant to assist your duelling weapon. You may hold this weapon and the 'duelling' trait will still work. If this is held alongside a duelling weapon, the duelling weapon no longer has a penalty for additional enemies being in range.",
   },
   {
     id: "multi-headed",
     name: "Multi-headed",
     description:
-      "Weapon has multiple heads for different purposes. Has multiple traits depending on the head. It takes 1 turn to swap between each head. Typical of halberds.",
+      "Weapon has multiple heads for different purposes. Has multiple traits depending on the head. It takes 1 turn to swap between each head.",
   },
   {
     id: "cavalry-weapon",
     name: "Cavalry-weapon",
     description:
-      "Weapon is meant to be used whilst atop a horse. If not atop a horse, gain -3d6 to hit enemies. If atop a horse, you can choose to charge with it! If charging, when moving to distance 0 of an enemy, your melee attack cannot be blocked.",
+      "Weapon is meant to be used whilst atop a horse or mount. If not atop a horse, gain -3d6 to hit enemies. If atop a horse, you can choose to charge with it! If charging, when moving to distance 0 of an enemy, deal an instance of unavoidable damage. Must be started from at least 2 distances away.",
   },
   {
     id: "concealable",
     name: "Concealable",
     description:
-      "The weapon is remarkably tiny, one can hide it away with ease. So long as it is holstered, enemies are not aware of it. Small size hampers damage; deals -1 damage than their tier would otherwise allow. Typical of kunais, butterfly knives and other extra short blades.",
+      "The weapon is remarkably tiny, one can hide it away with ease. So long as it is holstered, enemies are not aware of it.",
   },
   {
     id: "throwable",
     name: "Throwable",
     description:
-      "The weapon is meant, first and foremost, to be thrown. You can throw it 5 + STR distances away, and hitting the target is a Strength check, instead of Dexterity. Deals -1 damage when used as a melee weapon. Each additional throwable has weight.",
+      "The weapon is meant, first and foremost, to be thrown. You can throw it 5 + STR distances away, and hitting the target is a Strength check, instead of Dexterity. Deals -1 damage when used as a melee weapon, damage cannot be brought to 0 through this.",
   },
   {
     id: "barbed-hooked",
@@ -93,7 +105,25 @@ export const MELEE_TRAITS: MeleeTraitDefinition[] = [
     id: "razor-bladed",
     name: "Razor bladed",
     description:
-      `Weapon must be tier 2 or higher to get this. The weapon is extremely sharp, like a razor. If you have 3 successes over your opponent, roll a 1d5 to disable (NOT cut off!) a limb. 1-2, left or right arm respectively. 3-4, left or right leg respectively. 5, the head. Losing an arm makes the opponent drop their item; losing both means they can hold nothing. Losing a leg kicks the player down to the bottom of initiative; losing both makes them immobile. Losing a head is instant death. Mutually exclusive with barbed.`,
+      "The weapon is extremely sharp, like a razor. If you have 3 successes over your opponent, roll a 1d6 to disable (NOT cut off!) a limb. 1-2, left or right arm respectively. 3-4, left or right leg respectively. 5, the torso, and 6, the head. Losing an arm makes the opponent drop their item; losing both means they can hold nothing. Losing a leg kicks the player down to the bottom of initiative; losing both makes them immobile and knocks them to the floor. Hitting the torso makes the target take +1 damage from all sources. Losing the head is instant death. Mutually exclusive with barbed.",
+  },
+  {
+    id: "buckler-shield",
+    name: "Buckler shield",
+    description:
+      "Tiny shield that helps defending you in melee. You may hold this weapon and the 'duelling' trait will still work. Enemies have -1d6 to hit you.",
+  },
+  {
+    id: "shield",
+    name: "Shield",
+    description:
+      "A moderately sized shield, among the most common types used during medieval times. Kite, round, square, there's many kinds. Enemies have -3d6 to hit you.",
+  },
+  {
+    id: "board-shield",
+    name: "Board shield",
+    description:
+      "A massive board shield, when placed on the ground, it reaches all the way to your chin. Enemies have -6d6 to hit you. Can be planted onto the ground, serving as 2d6 cover.",
   },
 ];
 

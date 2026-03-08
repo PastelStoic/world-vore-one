c718b10c3dfee5fad452941adb7adb4e56499998
DocDoc005 <doccerino@gmail.com>
2026-03-08 00:38:38 -0300

    Update weapons.ts

--- data/weapons.ts
+++ data/weapons.ts
@@ -37,6 +37,26 @@ Overloaded chamber:
     reloadTurns: 4,
   },
   {
+    id: "Smith-and-Wesson-Safety-Hammerless",
+    name: "Smith & Wesson Safety Hammerless",
+    type: "Double-action revolver",
+    kind: "double-action-revolver",
+    nation: "civilian",
+    damage: "2",
+    ammo: 5,
+    rateOfFire: 1,
+    weight: 1,
+    pointCost: 0,
+    gimmicks: `One-handed:
+*Weapon can be held in one hand, with another tool in your free hand.
+Agile:
+*Can be fired even at distance 0. Can be unholstered and holstered at no action cost.
+Cylinder:
+*Reloads each bullet individually, taking 1 turn per bullet.`,
+    compatibleAttachmentIds: [...REVOLVER_ATTACHMENTS],
+    reloadsIndividually: true,
+  },
+  {
     id: "flamethrower",
     name: "Flamethrower",
     type: "Flamethrower",

24fb43f53a251c6a426a4ee00ec70b6480fdd0a9
PastelStoic <pastelstoic@proton.me>
2026-03-29 13:51:18 -0400

    Fixed formatting

--- data/factions.ts
+++ data/factions.ts
@@ -34,11 +34,9 @@ const FACTION_BONUS_DEFINITIONS: Partial<
   "ROLEPLAYER - The Church of Provisional Rations": {
     grantsPerkIds: ["free-range"],
   },
-  {
   "ROLEPLAYER - Gambling And Guts (GAG)": {
     grantsPerkIds: ["patron"],
   },
-  {
   "ROLEPLAYER - The Shinyakaze - Right Hand of the Goddess": {
     grantsPerkIds: ["hidden-personality"],
   },

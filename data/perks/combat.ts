5fbef61469d46096fb2c8fc68bae8a753a3aed83
DocDoc005 <doccerino@gmail.com>
2026-05-31 00:50:53 -0300

    Revise self-protection mechanics in combat perks
    Updated the mechanics for self-protection in combat, allowing up to three attacks to be protected per turn instead of one.

--- data/perks/combat.ts
+++ data/perks/combat.ts
@@ -189,7 +189,7 @@ export const COMBAT_PERKS: PerkDefinition[] = [
 
 *When being attacked, you may pull an ally/enemy in the same distance as you into the way. Does not apply if you were ambushed.
 *The target you pull must not be incapacitated/critical, only targets you can quickly reach out for and pull into your way are valid.
-*You may only protect yourself from a single attack per round, and it is a free action.
+*You may protect yourself from up to three attacks per turn, and they are all free actions.
 *Your target may be pulled before or after you perform your cover/defense roll.
 *If your target is willing, they take all of the damage in your stead.
 *If your target is unwilling, you may do a STR vs STR contested check against them. On a success, they take all of the damage in your stead.

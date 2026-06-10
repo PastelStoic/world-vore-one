983eae181aaf3e5a0f75bc6e01b4442ee4926cd5
DocDoc005 <doccerino@gmail.com>
2026-06-10 00:01:12 -0300

    Update combat.ts

--- data/perks/combat.ts
+++ data/perks/combat.ts
@@ -119,7 +119,8 @@ export const COMBAT_PERKS: PerkDefinition[] = [
 *Tier 0 and tier 1 weapons ( unarmed and makeshift weapons respectively ) deal +2 damage.
 *Ignore damage penalties whilst using those weapons such as armor or a lizardgirl's scales.
 *When attacking or defending with tier 0 and tier 1 weapons, gain +4d6 to attack with them and count successes on a 4 or above, instead of a 5 and above.
-*Does nothing for tier 2 ( military ) weapons.`,
+*Does nothing for tier 2 ( military ) weapons.
+*Does not apply to thrown makeshift weapons.`,
   },
   {
     id: "sapper",
@@ -219,6 +220,7 @@ export const COMBAT_PERKS: PerkDefinition[] = [
 
 *Every weapon and tool can be holsted, unholstered, grabbed or dropped as a free action.
 *You can dual wield pistols OR one-handed melees. You can attack with both weapons in one turn.
+*Your fists count as weapons for this purpose. You can punch someone twice, or hit them with a sword and punch them, etc.
 *When dual wielding, you cannot utilize alternative firing techniques, unless both weapons are one-handed and both share the alternative firing technique gimmick.
 *Hefties, when using two-handed weapons as if they were one handed, gain a base -3d6 to attack with their secondary weapon, and a cumulative -3d6 for every additional shot/swing done with it. No accuracy bonus from the extra shots.
 *In one turn, you can perform two reloading actions as if they were one, reloading both held weapons, or a single one.

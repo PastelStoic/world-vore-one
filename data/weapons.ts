5fafc8591304ee5b45342fb324e33fe6faee32da
DocDoc005 <doccerino@gmail.com>
2026-08-26 19:17:19 -0300

    Fix weapon type and kind for Lewis Gun

--- data/weapons.ts
+++ data/weapons.ts
@@ -267,8 +267,8 @@ export const WEAPONS: WeaponDefinition[] = [
   {
     id: "lewis-gun",
     name: "Lewis Automatic Machine Gun",
-    type: "Leavy Machinegun",
-    kind: "leavy-machinegun",
+    type: "Light Machinegun",
+    kind: "light-machinegun",
     nation: "Britain",
     damage: "3",
     ammo: 45,

d76edfd509a463dd2fd6bfce03b7819f637c78e4
DocDoc005 <doccerino@gmail.com>
2026-08-26 20:18:47 -0300

    Clarify grappling mechanics in 'vore' perk description
    Updated the description of the 'vore' perk to clarify the mechanics of grappling targets.

--- data/perks/vore.ts
+++ data/perks/vore.ts
@@ -307,7 +307,8 @@ If you have prey inside you, dead or alive:
     description:
       `You are a grabby one! Your touchy hands rarely let go of people.
 
-*Gain +6d6, counting successes on 4 or above, in order to grapple targets. Your targets must roll twice in order to escape your grapple, taking the worst result.
+*This perk only applies if both of your hands are free and usable. This perk is made invalid if you are lacking a hand.
+*Gain +3d6, counting successes on 4 or above, in order to grapple targets. Your targets must roll twice in order to escape your grapple, taking the worst result.
 *You may grapple up to [STRENGTH] targets in one action, and you may grapple them from 1 distance away, immediately bringing them to your distance if you succeed.
 *If a target manages to escape your stomach, they come out still grappled by you.`,
   },
